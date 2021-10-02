6e4409b597857625405f47a2699cb7891582a171
FaisITApps <itapp5.sti@gmail.com>
2021-10-03 00:05:30 +0700

    revision code

--- controller/task2-controller.js
+++ controller/task2-controller.js
@@ -7,19 +7,21 @@ const {
 } = process.env 
 
 const task2ListSearchMovie = async (req, res) => {
-    const   endPoint = 'http://www.omdbapi.com/?i=tt3896198&apikey=9f336746',
-            {keywords, page} = req.query;
-
-    const saveLog = await LogURL.create({UrlEndPoint: endPoint,Parameter: keywords})
-
-    const listMovie = await fetch(`${endPoint}&s=${keywords}&page=${page || 1}`,{method: 'GET'})
-    .then(res => res.json())
-    .catch(e => {
-        console.error({
-            'messsage': "Upss, somethings wrong with connections",
-            error: e
-        });
-    });
+    const   endPoint = `http://www.omdbapi.com/?i=tt3896198&apikey=${APIKEY}`,
+            {keywords, page} = req.query,
+            
+            // fetch data from OMDb
+            listMovie = await fetch(`${endPoint}&s=${keywords}&page=${page || 1}`,{method: 'GET'})
+                .then(res => res.json())
+                .catch(e => {
+                    console.error({
+                        'messsage': "Upss, somethings wrong with connections",
+                        error: e
+                    });
+                }),
+            
+            // Save log searching
+            saveLog = await LogURL.create({UrlEndPoint: endPoint,Parameter: keywords})
 
     if (Promise.all([listMovie, saveLog])) 
     return res.status(200).send({
