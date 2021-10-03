4f637789c8b9c11f60e109c5d6856a9ee5682524
FaisITApps <itapp5.sti@gmail.com>
2021-10-03 10:29:56 +0700

    add api detail movie

--- controller/task2-controller.js
+++ controller/task2-controller.js
@@ -4,31 +4,85 @@ const LogURL = require('../model').logSeacrh;
 
 const {
     APIKEY
-} = process.env 
+} = process.env;
+
+const apiUrl = `http://www.omdbapi.com/?apikey=${APIKEY}`;
 
 const task2ListSearchMovie = async (req, res) => {
-    const   endPoint = `http://www.omdbapi.com/?i=tt3896198&apikey=${APIKEY}`,
-            {keywords, page} = req.query,
-            
-            // fetch data from OMDb
-            listMovie = await fetch(`${endPoint}&s=${keywords}&page=${page || 1}`,{method: 'GET'})
-                .then(res => res.json())
-                .catch(e => {
-                    console.error({
-                        'messsage': "Upss, somethings wrong with connections",
-                        error: e
-                    });
-                }),
-            
-            // Save log searching
-            saveLog = await LogURL.create({UrlEndPoint: endPoint,Parameter: keywords})
 
-    if (Promise.all([listMovie, saveLog])) 
-    return res.status(200).send({
-        listMovie
-    })
+    const   {keywords, page} = req.query,
+            endPoint = `${apiUrl}&s=${keywords}&page=${page || 1}`;
+            
+            try {
+
+                // fetch data from OMDb
+                const listMovie = await fetch(`${endPoint}`,{method: 'GET'})
+                .then(res => res.json());
+        
+                // Save log searching
+                const saveLog = await LogURL.create({UrlEndPoint: endPoint,Parameter: keywords});
+
+                await Promise.all([listMovie, saveLog])
+                .then(([res1, res2]) => {
+                    return res.status(200).send({
+                        success: true,
+                        detailMovie
+                    })
+                })
+                .catch(([err1, err2]) => {
+                    return res.status(500).send({
+                        success: false,
+                        messsage: err1 || err2
+                    })
+                })
+                
+            } catch (error) {
+                return res.status(500).send({
+                    success: false,
+                    messsage: "Upss, somethings wrong with connections",
+                })
+            }
+            
+};
+
+const task2SearchMovieById = async (req, res) => {
+    
+    const   {id} = req.query,
+            endPoint = `${apiUrl}&i=${id}`;
+
+            try {
+
+                // fetch data from OMDb
+                const detailMovie = await fetch(`${endPoint}`,{method: 'GET'})
+                .then(res => res.json())
+        
+                // Save log searching
+                const saveLog = await LogURL.create({UrlEndPoint: endPoint,Parameter: id})
+    
+                await Promise.all([detailMovie, saveLog])
+                .then(([res1, res2]) => {
+                    return res.status(200).send({
+                        success: true,
+                        detailMovie
+                    })
+                })
+                .catch(([err1, err2]) => {
+                    return res.status(500).send({
+                        success: false,
+                        messsage: err1 || err2
+                    })
+                });
+
+            } catch (err) {
+                return res.status(500).send({
+                    success: false,
+                    messsage: err
+                })
+            }
+            
 };
 
 module.exports = {
-    task2ListSearchMovie
+    task2ListSearchMovie,
+    task2SearchMovieById
 }
--- routes/task2-router.js
+++ routes/task2-router.js
@@ -2,5 +2,6 @@ const router = require('express').Router();
 const movieController = require('../controller').task2ListSearchMovie;
 
 router.get('/search', movieController.task2ListSearchMovie);
+router.get('/detail', movieController.task2SearchMovieById);
 
 module.exports = router;
