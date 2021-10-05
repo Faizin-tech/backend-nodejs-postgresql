25ca3497a597ba1f7a8b746b97bfb212a7606b12
FaisITApps <itapp5.sti@gmail.com>
2021-10-05 14:24:11 +0700

    deploy

--- controller/task1-controller.js
+++ controller/task1-controller.js
@@ -18,28 +18,26 @@ const client = config.CLIENT
 
 const task1 =  async (req, res) => {
 
-    client.query(
+    sequileze.query(
         `
             INSERT INTO "Users" ("UserName", "Parent", "createdAt", "updatedAt") VALUES ('Ali', 2, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP);
             INSERT INTO "Users" ("UserName", "Parent", "createdAt", "updatedAt") VALUES ('Budi', 0, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP);
             INSERT INTO "Users" ("UserName", "Parent", "createdAt", "updatedAt") VALUES ('Cecep', 1, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP);
 
             SELECT "id" AS ID,"UserName",(SELECT "UserName" FROM "Users" B WHERE B."id" = A."Parent") AS UserParent FROM "Users" A
-        `
-        , (err, data) => {
-        if (err) {
-            console.log(err);
-            return res.status(400).send({
-                success: false,
-                message: "Can't load users"
-            });
-        } else {
-            return res.status(200).send({
-                success: true,
-                message: "Success Get Data",
-                response: data[3].rows
-            });
-        }
+        `)
+    .then((data) => {
+        return res.status(200).send({
+            success: true,
+            message: "Success Get Data",
+            response: data
+        });
+    })
+    .catch((error) => {
+        return res.status(400).send({
+            success: false,
+            message: "Can't load users"
+        });
     })
 
 }
