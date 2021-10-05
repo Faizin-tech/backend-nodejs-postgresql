3d5ff06503ee8e2dd3e9be0f5fbcee9f442f62c0
FaisITApps <itapp5.sti@gmail.com>
2021-10-05 08:47:54 +0700

    refactoring response error task 2

--- controller/task2-controller.js
+++ controller/task2-controller.js
@@ -15,21 +15,22 @@ const task2ListSearchMovie = async (req, res) => {
             
             try {
 
-                // fetch data from OMDb
-                const listMovie = await fetch(`${endPoint}`,{method: 'GET'})
-                .then(res => res.json());
-        
-                // Save log searching
-                const saveLog = await LogURL.create({UrlEndPoint: endPoint,Parameter: keywords});
-
+                        // fetch data from OMDb
+                const   response = await fetch(`${endPoint}`,{method: 'GET'}),
+                        listMovie = await response.json(),
+                        // Save log searching
+                        saveLog = await LogURL.create({UrlEndPoint: endPoint,Parameter: keywords});
+                
                 await Promise.all([listMovie, saveLog])
                 .then(([res1, res2]) => {
+                    console.log(res1,res2);
                     return res.status(200).send({
                         success: true,
                         listMovie
                     })
                 })
                 .catch(([err1, err2]) => {
+                    console.log('error nih');
                     return res.status(500).send({
                         success: false,
                         messsage: err1 || err2
@@ -40,6 +41,7 @@ const task2ListSearchMovie = async (req, res) => {
                 return res.status(500).send({
                     success: false,
                     messsage: "Upss, somethings wrong with connections",
+                    error: error.message
                 })
             }
             
@@ -52,12 +54,12 @@ const task2SearchMovieById = async (req, res) => {
 
             try {
 
-                // fetch data from OMDb
-                const detailMovie = await fetch(`${endPoint}`,{method: 'GET'})
-                .then(res => res.json())
-        
-                // Save log searching
-                const saveLog = await LogURL.create({UrlEndPoint: endPoint,Parameter: id})
+                        // fetch data from OMDb
+                const   response = await fetch(`${endPoint}`,{method: 'GET'}),
+                        detailMovie = await response.json(),
+                        
+                        // Save log searching
+                        saveLog = await LogURL.create({UrlEndPoint: endPoint,Parameter: id});
     
                 await Promise.all([detailMovie, saveLog])
                 .then(([res1, res2]) => {
